d1fec838449e2765d11fe97875c129166fe08a88
Arun Ganesan <arunganesan123@gmail.com>
2019-01-21 12:07:23 -0500

    [carlabui] Refactored view rendering using fragments

--- carlabui/src/main/java/edu/umich/carlabui/ExperimentBaseActivity.java
+++ carlabui/src/main/java/edu/umich/carlabui/ExperimentBaseActivity.java
@@ -7,6 +7,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.*;
@@ -19,17 +22,21 @@ import edu.umich.carlab.net.CheckUpdate;
 import edu.umich.carlab.recurring.UploadFiles;
 import edu.umich.carlab.utils.Utilities;
 
-import edu.umich.carlabui.Constants;
-
 import java.io.File;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static edu.umich.carlab.Constants.*;
 import static edu.umich.carlabui.AppsAdapter.AppState.ACTIVE;
 import static edu.umich.carlabui.Constants.ManualChoiceKey;
 
-public class ExperimentBaseActivity extends Activity {
-    Button  showMiddleware,
+public class ExperimentBaseActivity extends AppCompatActivity
+        implements InfoViewFragment.OnFragmentInteractionListener,
+            MiddlewareGridFragment.OnFragmentInteractionListener,
+            AppViewFragment.OnFragmentInteractionListener {
+    Button showMiddleware,
             manualOnOffToggle,
             pauseCarlab,
             saveCurrentTrace,
@@ -42,91 +49,10 @@ public class ExperimentBaseActivity extends Activity {
 
     SharedPreferences prefs;
     CLService carlabService;
-    List<AppsAdapter.AppModel> appModels;
-    Map<String, Integer> appModelIndexMap;
-    AppsAdapter appsAdapter;
     FrameLayout mainWrapper;
 
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.experiment_container);
-        prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        appModels = new ArrayList<>();
-        appModelIndexMap = new HashMap<>();
-
-        wireUI();
-        loadAndInitializeInfo();
-        updatePauseButton();
-        Utilities.scheduleOnce(this, ManualTrigger.class, 0);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        updateTriggerButton();
-        updatePauseButton();
-        bindService(
-                new Intent(
-                        this,
-                        CLService.class),
-                mConnection,
-                Context.BIND_AUTO_CREATE);
-
-        registerReceiver(appStateReceiver, appStateIntentFilter);
-        registerReceiver(clStopped, new IntentFilter(CLSERVICE_STOPPED));
-        registerReceiver(updateReceiver, new IntentFilter(STATUS_CHANGED));
-        registerReceiver(clStarted, new IntentFilter(DONE_INITIALIZING_CL));
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        unbindService(mConnection);
-
-        unregisterReceiver(appStateReceiver);
-        unregisterReceiver(clStopped);
-        unregisterReceiver(updateReceiver);
-        unregisterReceiver(clStarted);
-    }
-
-    void wireUI() {
-        mainWrapper = findViewById(R.id.main_wrapper);
-
-        showMiddleware = findViewById(R.id.showMiddleware);
-        showMiddleware.setOnClickListener(loadMiddlewareActivity);
-
-        showInfo = findViewById(R.id.showInfo);
-        showInfo.setOnClickListener(loadInfoActivity);
-
-        downloadUpdate = (Button) findViewById(R.id.downloadUpdate);
-        downloadUpdate.setOnClickListener(downloadUpdateCallback);
-
-        uploadFiles = (Button) findViewById(R.id.uploadTrips);
-        uploadFiles.setOnClickListener(uploadFilesCallback);
-
-        manualOnOffToggle = (Button) findViewById(R.id.toggleCarlab);
-        manualOnOffToggle.setOnClickListener(toggleCarlab);
-
-        pauseCarlab = (Button)findViewById(R.id.pauseCarlab);
-        pauseCarlab.setOnClickListener(togglePauseCarlab);
-    }
-
-
-    void loadAndInitializeInfo() {
-        LayoutInflater inflater = getLayoutInflater();
-        View infoView = inflater.inflate(R.layout.experiment_info, null);
-        TextView uidText = infoView.findViewById(R.id.uid_text);
-        String UID = prefs.getString(UID_key, "UNDEFINED");
-        uidText.setText("UID: " + UID);
-
-        mainWrapper.removeAllViews();
-        mainWrapper.addView(infoView);
-    }
-
+    InfoViewFragment infoFragment = new InfoViewFragment();
+    MiddlewareGridFragment middlewareGridFragment = new MiddlewareGridFragment();
 
     /********************** Receivers for CarLab and status changes **********/
     BroadcastReceiver updateReceiver = new BroadcastReceiver() {
@@ -135,20 +61,17 @@ public class ExperimentBaseActivity extends Activity {
             updatePauseButton();
         }
     };
-
     BroadcastReceiver clStopped = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
         }
     };
-
     BroadcastReceiver clStarted = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             updatePauseButton();
         }
     };
-
     /************************* UI callback functions *************************/
     View.OnClickListener toggleCarlab = new View.OnClickListener() {
         @Override
@@ -161,7 +84,6 @@ public class ExperimentBaseActivity extends Activity {
                     ManualTrigger.class));
         }
     };
-
     View.OnClickListener togglePauseCarlab = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -181,29 +103,6 @@ public class ExperimentBaseActivity extends Activity {
             sendBroadcast(new Intent(STATUS_CHANGED));
         }
     };
-
-    void updateTriggerButton() {
-        boolean isOn = prefs.getBoolean(ManualChoiceKey, false);
-        manualOnOffToggle.setText(isOn ? "Turn Off" : "Turn On");
-    }
-
-
-    void updatePauseButton() {
-        TriggerSession.SessionState sessionState = TriggerSession.SessionState.values()[prefs.getInt(edu.umich.carlab.Constants.Session_State_Key, 1)];
-        if (sessionState == TriggerSession.SessionState.OFF) {
-            pauseCarlab.setText(getString(R.string.carlab_running_button));
-            pauseCarlab.setEnabled(false);
-        } else {
-            if (sessionState == TriggerSession.SessionState.ON) {
-                pauseCarlab.setText(getString(R.string.carlab_running_button));
-            } else {
-                pauseCarlab.setText(getString(R.string.carlab_paused_button));
-            }
-            pauseCarlab.setEnabled(true);
-        }
-    }
-
-
     View.OnClickListener uploadFilesCallback = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -212,7 +111,6 @@ public class ExperimentBaseActivity extends Activity {
             sendBroadcast(new Intent(ExperimentBaseActivity.this, UploadFiles.class));
         }
     };
-
     View.OnClickListener downloadUpdateCallback = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -248,105 +146,17 @@ public class ExperimentBaseActivity extends Activity {
             }
         }
     };
-
-
-
-    View.OnClickListener loadMiddlewareActivity = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            LayoutInflater inflater = getLayoutInflater();
-            FrameLayout middlewareGridLayout = (FrameLayout) inflater.inflate(R.layout.middleware_grid, null);
-            GridView middlewareGrid = middlewareGridLayout.findViewById(R.id.middleware_grid);
-
-            appModels.clear();
-
-            // If CarLab is running, we can create live middleware links
-            // Otherwise, we will just use the static app loader
-            List<App> allApps = AppLoader.getInstance().instantiateApps(null, null);
-
-            for (App app : allApps) {
-                AppsAdapter.AppState appState = ACTIVE;
-                appModels.add(new AppsAdapter.AppModel(
-                        app.getName(),
-                        app.getClass().getCanonicalName(),
-                        appState));
-            }
-
-            // This index is useful later
-            for (int i = 0; i < appModels.size(); i++) {
-                String classname = appModels.get(i).className;
-                appModelIndexMap.put(classname, i);
-            }
-
-            appsAdapter = new AppsAdapter(ExperimentBaseActivity.this, appModels);
-            middlewareGrid.setAdapter(appsAdapter);
-            middlewareGrid.setOnItemClickListener(openAppDetails);
-            mainWrapper.removeAllViews();
-            mainWrapper.addView(middlewareGridLayout);
-        }
-    };
-
-    private AdapterView.OnItemClickListener openAppDetails = new AdapterView.OnItemClickListener() {
-        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-            if (carlabService == null) return;
-
-            AppsAdapter.AppModel appModel = appModels.get(position);
-            App app = carlabService.getRunningApp(appModel.className);
-            if (app == null) return;
-
-            LayoutInflater inflater = getLayoutInflater();
-            View middlewareWrapper = inflater.inflate(R.layout.middleware_wrapper, null);
-            TextView middlewareTitle = middlewareWrapper.findViewById(R.id.middleware_title);
-            middlewareTitle.setText(app.getName());
-
-            FrameLayout middlewareContent = middlewareWrapper.findViewById(R.id.middleware_content);
-            View appView = app.initializeVisualization(ExperimentBaseActivity.this);
-            if (appView != null) middlewareContent.addView(appView);
-            mainWrapper.removeAllViews();
-            mainWrapper.addView(middlewareWrapper);
-        }
-    };
-
-
     View.OnClickListener loadInfoActivity = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             loadAndInitializeInfo();
         }
     };
-    /*************************************************************************/
 
-    /**
-     * Once we receive any app state updates, we will change the app state.
-     */
-    IntentFilter appStateIntentFilter = new IntentFilter(edu.umich.carlab.Constants.INTENT_APP_STATE_UPDATE);
-    private BroadcastReceiver appStateReceiver = new BroadcastReceiver() {
+    View.OnClickListener loadMiddlewareActivity = new View.OnClickListener() {
         @Override
-        public void onReceive(Context context, Intent intent) {
-            //activeApps = prefs.getStringSet(Constants.ACTIVE_APPS_KEY, new HashSet<String>());
-
-            String appClassName = intent.getStringExtra("appClassName");
-            DataMarshal.MessageType appState = (DataMarshal.MessageType) intent.getSerializableExtra("appState");
-
-            // This means we haven't set up this app yet
-            if (appModelIndexMap == null) return;
-            if (!appModelIndexMap.containsKey(appClassName))
-                return;
-
-            int appIndex = appModelIndexMap.get(appClassName);
-
-            switch (appState) {
-                case ERROR:
-                    appModels.get(appIndex).state = AppsAdapter.AppState.ERROR;
-                    break;
-                case DATA:
-                    appModels.get(appIndex).state = AppsAdapter.AppState.DATA;
-                    break;
-                case STATUS:
-                    appModels.get(appIndex).state = AppsAdapter.AppState.PROCESSING;
-            }
-
-            appsAdapter.notifyDataSetChanged();
+        public void onClick(View view) {
+           replaceFragmentWithAnimation(middlewareGridFragment, "MIDDLEWARE");
         }
     };
 
@@ -365,7 +175,107 @@ public class ExperimentBaseActivity extends Activity {
         public void onServiceDisconnected(ComponentName arg0) {
             carlabService = null;
         }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.experiment_container);
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+
+        wireUI();
+        loadAndInitializeInfo();
+        updatePauseButton();
+        Utilities.scheduleOnce(this, ManualTrigger.class, 0);
     }
-    ;
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateTriggerButton();
+        updatePauseButton();
+        bindService(
+                new Intent(
+                        this,
+                        CLService.class),
+                mConnection,
+                Context.BIND_AUTO_CREATE);
+
+        registerReceiver(clStopped, new IntentFilter(CLSERVICE_STOPPED));
+        registerReceiver(updateReceiver, new IntentFilter(STATUS_CHANGED));
+        registerReceiver(clStarted, new IntentFilter(DONE_INITIALIZING_CL));
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        unbindService(mConnection);
+
+        unregisterReceiver(clStopped);
+        unregisterReceiver(updateReceiver);
+        unregisterReceiver(clStarted);
+    }
+
+    void wireUI() {
+        mainWrapper = findViewById(R.id.main_wrapper);
+
+        showMiddleware = findViewById(R.id.showMiddleware);
+        showMiddleware.setOnClickListener(loadMiddlewareActivity);
+
+        showInfo = findViewById(R.id.showInfo);
+        showInfo.setOnClickListener(loadInfoActivity);
+
+        downloadUpdate = (Button) findViewById(R.id.downloadUpdate);
+        downloadUpdate.setOnClickListener(downloadUpdateCallback);
+
+        uploadFiles = (Button) findViewById(R.id.uploadTrips);
+        uploadFiles.setOnClickListener(uploadFilesCallback);
+
+        manualOnOffToggle = (Button) findViewById(R.id.toggleCarlab);
+        manualOnOffToggle.setOnClickListener(toggleCarlab);
+
+        pauseCarlab = (Button) findViewById(R.id.pauseCarlab);
+        pauseCarlab.setOnClickListener(togglePauseCarlab);
+    }
+
+    // https://stackoverflow.com/questions/4932462/animate-the-transition-between-fragments
+    public void replaceFragmentWithAnimation(Fragment fragment, String tag) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        transaction.replace(R.id.main_wrapper, fragment);
+        transaction.addToBackStack(tag);
+        transaction.commit();
+    }
     /*************************************************************************/
+
+    void loadAndInitializeInfo() {
+        replaceFragmentWithAnimation(infoFragment, "TAG");
+    }
+
+    void updateTriggerButton() {
+        boolean isOn = prefs.getBoolean(ManualChoiceKey, false);
+        manualOnOffToggle.setText(isOn ? "Turn Off" : "Turn On");
+    }
+
+    void updatePauseButton() {
+        TriggerSession.SessionState sessionState = TriggerSession.SessionState.values()[prefs.getInt(edu.umich.carlab.Constants.Session_State_Key, 1)];
+        if (sessionState == TriggerSession.SessionState.OFF) {
+            pauseCarlab.setText(getString(R.string.carlab_running_button));
+            pauseCarlab.setEnabled(false);
+        } else {
+            if (sessionState == TriggerSession.SessionState.ON) {
+                pauseCarlab.setText(getString(R.string.carlab_running_button));
+            } else {
+                pauseCarlab.setText(getString(R.string.carlab_paused_button));
+            }
+            pauseCarlab.setEnabled(true);
+        }
+    }
+    /*************************************************************************/
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+    }
 }
