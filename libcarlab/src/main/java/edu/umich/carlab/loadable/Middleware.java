fcbd42be73d208e4918c5e9a851bff6ca704581b
Arun Ganesan <arunganesan123@gmail.com>
2019-02-19 11:51:02 -0500

    Added string-based Middleware parameters

--- libcarlab/src/main/java/edu/umich/carlab/loadable/Middleware.java
+++ libcarlab/src/main/java/edu/umich/carlab/loadable/Middleware.java
@@ -23,10 +23,22 @@ public abstract class Middleware {
         prefs.edit().putFloat(namespaceKey, value).apply();
     }
 
+    public void setParameter(Context context, String key, String value) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        String namespaceKey = String.format("%s:%s", getName(), key);
+        prefs.edit().putString(namespaceKey, value).apply();
+    }
+
     public Float getParameterOrDefault(Context context, String key, Float defaultVal) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         String namespaceKey = String.format("%s:%s", getName(), key);
         return prefs.getFloat(namespaceKey, defaultVal);
+    }
+
+    public String getParameterOrDefault(Context context, String key, String defaultVal) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        String namespaceKey = String.format("%s:%s", getName(), key);
+        return prefs.getString(namespaceKey, defaultVal);
     }
 
     public Map<String, Float> splitValues(DataMarshal.DataObject dataObject) {
